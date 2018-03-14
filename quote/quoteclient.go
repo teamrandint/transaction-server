2f9fb224c92b2565111a76aad8bebf12f3908217
shaebrown <shaeb@uvic.ca>
2018-03-13 21:40:49 -0700

    Extracted quotes to its own server

--- quote/quoteclient.go
+++ quote/quoteclient.go
@@ -1,89 +1,39 @@
 package quoteclient
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"regexp"
-	"seng468/transaction-server/logger"
-	"strconv"
-	"time"
-
-	"github.com/patrickmn/go-cache"
 	"github.com/shopspring/decimal"
+	"os"
+	"net/http"
+	"log"
+	"fmt"
+	"strconv"
+	"io/ioutil"
 )
 
-type QuoteClientI interface {
-	Query(string, string, int) (decimal.Decimal, error)
-}
+var addr = os.Getenv("quoteaddr")
+var port = os.Getenv("quoteport")
 
-type QuoteClient struct {
-	name   string
-	addr   string
-	cache  *cache.Cache
-	re     *regexp.Regexp
-	logger logger.Logger
-}
-
-type QuoteReply struct {
-	quote decimal.Decimal
-	stock string
-	user  string
-	time  uint64
-	key   string
-}
-
-func NewQuoteClient(logger logger.Logger) *QuoteClient {
-	re := regexp.MustCompile("(?P<quote>.+),(?P<stock>.+),(?P<user>.+),(?P<time>.+),(?P<key>.+)")
-	return &QuoteClient{
-		name:   "quoteserve",
-		addr:   "quoteserve.seng:4444",
-		cache:  cache.New(time.Minute, time.Minute),
-		re:     re,
-		logger: logger,
-	}
-}
-
-func (q *QuoteClient) Query(u string, s string, transNum int) (decimal.Decimal, error) {
-	quote, found := q.cache.Get(s)
-	if found {
-		d, _ := decimal.NewFromString(quote.(string))
-		return d, nil
-	}
-	conn, err := net.DialTimeout("tcp", q.addr, 30*time.Millisecond)
+func Query(user string, stock string, transNum int) (decimal.Decimal, error) {
+	req, err := http.NewRequest("GET", addr + ":" + port + "/quote", nil)
 	if err != nil {
+		log.Print(err)
+	}
+	q := req.URL.Query()
+	q.Add("user", user)
+	q.Add("stock", stock)
+	q.Add("transNum", strconv.Itoa(transNum))
+	req.URL.RawQuery = q.Encode()
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error connecting to the quote server: %s", err.Error())
 		return decimal.Decimal{}, err
 	}
-	request := fmt.Sprintf("%s,%s\n", s, u)
-	fmt.Fprintf(conn, request)
-	message, err := bufio.NewReader(conn).ReadString('\n')
+	amount, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		fmt.Printf("Error reading body: %s", err.Error())
 		return decimal.Decimal{}, err
 	}
-	reply := q.getReply(message)
-	go q.logger.QuoteServer(q.name, transNum, reply.quote.String(), reply.stock,
-		reply.user, reply.time, reply.key)
-	q.cache.Set(reply.stock, reply.quote.String(), cache.DefaultExpiration)
-	conn.Close()
-	return reply.quote, nil
-}
-
-func (q *QuoteClient) getReply(msg string) *QuoteReply {
-	n1 := q.re.SubexpNames()
-	r2 := q.re.FindAllStringSubmatch(msg, -1)[0]
-
-	res := map[string]string{}
-	for i, n := range r2 {
-		res[n1[i]] = n
-	}
-
-	quote, _ := decimal.NewFromString(res["quote"])
-	timestamp, _ := strconv.ParseUint(res["time"], 10, 64)
-	return &QuoteReply{
-		quote: quote,
-		stock: res["stock"],
-		user:  res["user"],
-		time:  timestamp,
-		key:   res["key"],
-	}
+	return decimal.NewFromString(string(amount))
 }
